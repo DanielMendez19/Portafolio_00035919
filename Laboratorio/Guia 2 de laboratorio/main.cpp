d5a75e49c4564023f090030c207da24c42e85832
Daniel Alberto Rodriguez Mendez <00035919@uca.edu.sv>
2019-09-13 16:52:11 -0600

    tercer commit2

--- Laboratorio/Guia 2 de laboratorio/main.cpp
+++ Laboratorio/Guia 2 de laboratorio/main.cpp
@@ -1,175 +1,101 @@
 //
 //  main.cpp
-//  actividad 8 de portafolio
+//  guia2
 //
 //  Created by Saul Enrique Coreas Mendez on 9/13/19.
 //  Copyright © 2019 Daniel Alberto Rodriguez Mendez. All rights reserved.
 //
 
+
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-//////////////////////dejar vacia//////
- struct Tpila{
- float elements[100];
- int top;
- };
- typedef struct Tpila Pila;
- 
- void initialize(Pila *s){
- s->top = -1;
- }
- 
- bool empty(Pila *s){
- return s->top < 0;
- }
- 
- void push(Pila *s, float e){
- if(s->top < 99){
- (s->top)++;
- s->elements[s->top] = e;
- }
- }
- 
- void pop(Pila *s, float *e){
- if(s->top >= 0){
- *e = s->elements[s->top];
- (s->top)--;
- }
- }
+///////////////Ejercicio 1//////////
+/*
+int valornormal(int a,int b){
+    int u=0;
+    int u1=0;
+    u=a%b;
+    if(u==0){
+    return b;
+    }
+    
+    while(u==0){
+        u1=b%u;
+    }
+    return u1;
+    
+    
+    
+    return 0;
+}
 
-int main()
-{
-    Pila unaPila;
-    initialize(&unaPila);
+int valoralreves(int w,int z){
+    int v=0;
+    int v1=0;
+    v=z%w;
+    if(v==0){
+    return w;
+    }
+    while(v==0){
+        v1=w%v;
+    }
+    return v1;
     
-    if(empty(&unaPila))
-        cout << "Esta vacia" << endl;
+    return 0;
+}
+
+int main() {
+    int x=0;
+    int y=0;
+    int p=0;
+    int c=0;
     
-    float x = 5;
-    float x1 = 4;
-    float x2 = 3;
-    float x3 = 2;
-    float x4 = 1;
+    cout<<"Introduzca los dos numeros"<<endl;
+    cout<<"Primero: "<<endl;
+    cin>>x;
+    cout<<"Segundo: "<<endl;
+    cin>>y;
+    if(x==0||y==0){
+        return 0;
+    }
+    
+    p = valornormal(x,y);
+    c = valoralreves(x,y);
     
     
-    push(&unaPila, x);
-    push(&unaPila, x1);
-    push(&unaPila, x2);
-    push(&unaPila, x3);
-    push(&unaPila, x4);
+    if(x>y){
+        
+        cout<<"mcd: "<< p <<endl;
+    }else{
+        cout<<"mcd: "<< c <<endl;
+    }
     
-    if(empty(&unaPila))
-        cout << "Esta vacia" << endl;
     
-    float y = 0;
-    float y1 = 0;
-    float y2 = 0;
-    float y3 = 0;
-    float y4 = 0;
-    
-    pop(&unaPila, &y4);
-    pop(&unaPila, &y3);
-    pop(&unaPila, &y1);
-    pop(&unaPila, &y2);
-    pop(&unaPila, &y);
-    
-    cout<<" valor del fondo: "<<y<<endl;
-    
-    if(empty(&unaPila))
-        cout << "Esta vacia" << endl;
-    
-    while(!empty(&unaPila)){
-        pop(&unaPila, &y);
-        cout << "y: " << y << endl;
+    return 0;
+}
+
+ */
+
+////////////////////Ejercicio 8/////
+/*
+void mostrar(int n){
+    if(n==0)
+        cout << n << endl;
+    else{
+        cout << n << endl;
+        mostrar(n-1);
     }
 }
 
-/////////////dejar igual////////
-/*
- struct Tpila{
- float elements[100];
- int top;
- };
- typedef struct Tpila Pila;
- 
- void initialize(Pila *s){
- s->top = -1;
- }
- 
- bool empty(Pila *s){
- return s->top < 0;
- }
- 
- void push(Pila *s, float e){
- if(s->top < 99){
- (s->top)++;
- s->elements[s->top] = e;
- }
- }
- 
- void pop(Pila *s, float *e){
- if(s->top >= 0){
- *e = s->elements[s->top];
- (s->top)--;
- }
- }
- 
- int main()
- {
- Pila unaPila;
- initialize(&unaPila);
- 
- if(empty(&unaPila))
- cout << "Esta vacia" << endl;
- 
- float x = 5;
- float x1 = 4;
- float x2 = 3;
- float x3 = 2;
- float x4 = 1;
- 
- 
-     
- push(&unaPila, x);
- push(&unaPila, x1);
- push(&unaPila, x2);
- push(&unaPila, x3);
- push(&unaPila, x4);
- 
- if(empty(&unaPila))
- cout << "Esta vacia" << endl;
- 
- float y = 0;
- float y1 = 0;
- float y2 = 0;
- float y3 = 0;
- float y4 = 0;
- 
- pop(&unaPila, &y4);
- pop(&unaPila, &y3);
- pop(&unaPila, &y1);
- pop(&unaPila, &y2);
- pop(&unaPila, &y);
- 
- 
-     push(&unaPila, x4);
-     push(&unaPila, x3);
-     push(&unaPila, x2);
-     push(&unaPila, x1);
-     push(&unaPila, x);
-     
- 
-     cout<<" valor del fondo: "<<y<<endl;
-     
- while(!empty(&unaPila)){
- pop(&unaPila, &y);
- cout << "y: " << y << endl;
- }
-     
-     
- }
-
-
+int main()
+{
+    int x = 0;
+    cout << "Digite un numero entero: ";
+    cin >> x;
+    if(x>=0)
+        mostrar(x);
+}
 */
-
