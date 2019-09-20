346914598ba246cad3cc531f5fc6ff3ab6cb2861
Daniel Alberto Rodriguez Mendez <00035919@uca.edu.sv>
2019-09-19 18:18:01 -0600

    cuarto commit1

--- Actividad de portafolio 9/main.cpp
+++ Actividad de portafolio 9/main.cpp
@@ -1,74 +1,38 @@
 //
 //  main.cpp
-//  act de portafolio 6
+//  9 actividad de portafolio
 //
-//  Created by Saul Enrique Coreas Mendez on 9/13/19.
+//  Created by Saul Enrique Coreas Mendez on 9/19/19.
 //  Copyright © 2019 Daniel Alberto Rodriguez Mendez. All rights reserved.
 //
 
 #include <iostream>
+#include <stack>
 using namespace std;
 
 
+int main()
+{
+    stack<int> s;
 
-int rec(int y,int low, int high, int *a){
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    
+    cout << "Elemento de ultimo: " << s.top() << endl;
     
     
-    
-    
-   
-  
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop();
 
-int rec(int y ,int *a){
-    int low=0;
-    int high=0;
     
-    if(low>high){
-        return -1;
-    }
-    
-    int mid = (low+high)/2;
-    
-    if(y == a[mid]){
-        
-        return mid;
-    }
-    if(y < a[mid])
-        
-        for (int i = 0; low < mid-1; i--) {
-            if(y == a[i]){
-                int indice = -1;
-                indice = y;
-            }
-        }
-        
+    if (s.empty())
+        cout << "Pila esta vacia"<<endl;
     else
-        for (int i = 0; mid+1 < high; i++) {
-            if(y == a[i]){
-                int indice = -1;
-                indice = y;
-            }
-            }
+        cout << "Pila no esta vacia"<<endl;
     
-    
+    return 0;
 }
-
-
-    int main(){
-        int a[9]={11,3,4,5,17,18,31,3};
-        int low = 0;
-        int high = a[8]-1;
-        int i = 0;
-        //int x = 0;
-        
-        cout<<"digite un numero"<<endl;
-        cin>>i;
-        
-        if(i == -1){
-            cout<<"No se encontro"<<endl;
-            
-        }else{
-            cout<<"El elemento se encuentra en la casilla "<<i<<endl;
-        }
-        
-    }
